2d5d37ec5d65657b265f7b5cb3c533008fc11579
meatball <mb0520@sinyi-tech.com>
2017-04-27 14:00:26 +0800

    init edit

--- backend.py
+++ backend.py
@@ -14,6 +14,11 @@ def topic():
     return render_template("topic.html")
 
 
+@app.route('/edit')
+def edit():
+    return render_template("edit.html")
+
+
 if __name__ == '__main__':
     """
     browser-sync start -p "127.0.0.1:5000" -f "../cheatsheet_generate"
